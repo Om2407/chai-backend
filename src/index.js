88685484b4bbd2f08974be6104769a79bda31a4a
Om Gupta <your_email@example.com>
2025-09-11 06:05:20 +0530

    before Multer

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// require("dotenv").config();
+
+// import { DB_NAME } from "./constants";
+// import mongoose from "mongoose";
+
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './env'
+});
+
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect to the database", err);
+    });
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express()
+// ( async () =>{
+//     try{
+//        await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error", () =>{
+//         console.log("ERROR: ",error)
+//         throw error
+//        })
+//        app.listen(process.env.PORT,() =>{
+//         console.log(`Server is running on port ${process.env.PORT}`)
+//        })
+//     } catch(error){
+//         console.error("ERROR: ",error)
+//         throw error
+//     }
+// })()
